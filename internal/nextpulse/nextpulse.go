fbd65032eb97789589bc8cc153362511f24f1506
Marcos Mota <60157172+marcmotta@users.noreply.github.com>
2025-07-27 12:45:19 +0700

    Add nextpulse.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ internal/nextpulse/nextpulse.go
@@ -0,0 +1,36 @@
+// internal/nextpulse/nextpulse.go
+package nextpulse
+
+import (
+	"log"
+	"os"
+)
+
+type App struct {
+	verbose bool
+	logger  *log.Logger
+}
+
+func NewApp(verbose bool) *App {
+	app := &App{
+		verbose: verbose,
+		logger:  log.New(os.Stdout, "", log.LstdFlags),
+	}
+
+	if verbose {
+		app.logger.SetPrefix("[DEBUG] ")
+	} else {
+		app.logger.SetPrefix("[INFO] ")
+	}
+
+	return app
+}
+
+func (a *App) Run() error {
+	a.logger.Printf("Starting NextPulse processing")
+
+	// Add your implementation here
+
+	a.logger.Println("Processing completed successfully")
+	return nil
+}
